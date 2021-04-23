60052d55fc706b1e70ee86e4da8450d24c7fc92c
Sreyom31 <sreyom31@gmail.com>
2021-04-23 20:12:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+
+const tl = new TimelineMax();
+tl.fromTo(hero,1,{height:"0%"},{height:"80%",ease:Power2.easeInOut})
+.fromTo(hero,1.2,{width:"100%"},{width:"80%",ease:Power2.easeInOut})
+.fromTo(slider,1.2,{x:"-100%"},{x:"0%",ease:Power2.easeInOut});
